cf95365a2be2cff32224a5a6d9c18d791712e763
VanessaMtzMtz <78155041+VanessaMtzMtz@users.noreply.github.com>
2021-11-21 20:23:29 -0600

    AgregarBotones
    Se agregaron los botones de las escenas principales

---
+++ ProyectoRV/Assets/Scripts/ExitControl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitControl : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
